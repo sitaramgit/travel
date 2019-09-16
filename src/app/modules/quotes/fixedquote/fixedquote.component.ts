e34c36f262ae5413fbda7cdf3c4576549c097e2c
Manasavtigress <42064947+Manasavtigress@users.noreply.github.com>
2019-09-16 13:02:16 +0530

    Quotes Changes

---
+++ src/app/modules/quotes/fixedquote/fixedquote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fixedquote',
+  templateUrl: './fixedquote.component.html',
+  styleUrls: ['./fixedquote.component.css']
+})
+export class FixedquoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
